786f525f1c17c8ebc2fdf951e651b3c598f64742
p@p.p <p@p.p>
2022-09-27 16:03:47 +0200

    element can be added to a list, element can be removed, elemenet can be moved up and down.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { useState } from "react";
+
+function ShoppingList() {
+	const [inputValue, setInputValue] = useState("add items: ");
+	const [addElement, setAddElement] = useState([]);
+
+	const doList = addElement.map((element, index) => {
+		return (
+			<li>
+				{element}
+				<button
+					onClick={() => {
+						const del = [...addElement];
+						del.splice(index, 1);
+						setAddElement([...del]);
+					}}>
+					del
+				</button>
+				<button
+					disabled={index === 0}
+					onClick={() => {
+						const tempVar = element;
+						const prevVar = addElement[index - 1];
+						addElement[index - 1] = tempVar;
+						addElement[index] = prevVar;
+						setAddElement([...addElement]);
+					}}>
+					⬆
+				</button>
+				<button
+					disabled={index === addElement.length - 1}
+					onClick={() => {
+						const tempVar = element;
+						const prevVar = addElement[index + 1];
+						addElement[index + 1] = tempVar;
+						addElement[index] = prevVar;
+						setAddElement([...addElement]);
+					}}>
+					⬇
+				</button>
+			</li>
+		);
+	});
+
+	return (
+		<div>
+			<h1>Shopping List</h1>
+			<textarea>This is your personal shopping list</textarea>
+			<div id='ramka'>{doList}</div>
+			<div className='show'>{inputValue}</div>
+			<input
+				value={inputValue}
+				onChange={element => {
+					setInputValue(element.target.value);
+				}}
+			/>
+			<button
+				onClick={() => {
+					setAddElement([...addElement, inputValue]);
+					setInputValue(" ");
+				}}>
+				add
+			</button>
+		</div>
+	);
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<ShoppingList />
+		</div>
+	);
 }
 
 export default App;
