171dc24312bf3b6e4f8d937a506e6fb1b285f615
p@p.p <p@p.p>
2022-09-30 18:10:27 +0200

    zmiany

--- src/App.js
+++ src/App.js
@@ -3,54 +3,58 @@ import React from "react";
 import { useState } from "react";
 import { renderIntoDocument } from "react-dom/test-utils";
 
+function ShoppingEntry(props) {
+	return (
+		<li>
+			{props.element}
+			<button
+				onClick={() => {
+					const del = [...leftStack];
+					del.splice(index, 1);
+					props.setLeftStack([...del]);
+				}}>
+				del
+			</button>
+			<button
+				disabled={index === 0}
+				onClick={() => {
+					const tempVar = element;
+					const prevVar = leftStack[index - 1];
+					leftStack[index - 1] = tempVar;
+					leftStack[index] = prevVar;
+					setLeftStack([...leftStack]);
+				}}>
+				⬆
+			</button>
+			<button
+				disabled={index === leftStack.length - 1}
+				onClick={() => {
+					const tempVar = element;
+					const prevVar = leftStack[index + 1];
+					leftStack[index + 1] = tempVar;
+					leftStack[index] = prevVar;
+					setLeftStack([...leftStack]);
+				}}>
+				⬇
+			</button>
+			<button
+				onClick={() => {
+					setRightStack([...rightStack, element]);
+				}}>
+				➡
+			</button>
+		</li>
+	);
+}
+
 function ShoppingList() {
 	const [inputValue, setInputValue] = useState("add items: ");
 	const [leftStack, setLeftStack] = useState([]);
 	const [rightStack, setRightStack] = useState([]);
 
-	const mappedAddElement = leftStack.map((element, index) => {
-		return (
-			<li>
-				{element}
-				<button
-					onClick={() => {
-						const del = [...leftStack];
-						del.splice(index, 1);
-						setLeftStack([...del]);
-					}}>
-					del
-				</button>
-				<button
-					disabled={index === 0}
-					onClick={() => {
-						const tempVar = element;
-						const prevVar = leftStack[index - 1];
-						leftStack[index - 1] = tempVar;
-						leftStack[index] = prevVar;
-						setLeftStack([...leftStack]);
-					}}>
-					⬆
-				</button>
-				<button
-					disabled={index === leftStack.length - 1}
-					onClick={() => {
-						const tempVar = element;
-						const prevVar = leftStack[index + 1];
-						leftStack[index + 1] = tempVar;
-						leftStack[index] = prevVar;
-						setLeftStack([...leftStack]);
-					}}>
-					⬇
-				</button>
-				<button
-					onClick={() => {
-						setRightStack([...rightStack, element]);
-					}}>
-					➡
-				</button>
-			</li>
-		);
-	});
+	const mappedAddElement = leftStack.map((element, index) => (
+		<ShoppingEntry element={element} index={index} leftStack={leftStack} />
+	));
 
 	return (
 		<div>
